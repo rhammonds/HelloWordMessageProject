072bff1a9dcf615163a0e91fe08bf86b80bb8b30
Ricky Hammonds <mail@rhammonds.com>
2020-11-10 14:07:01 -0500

    Add files via upload

---
+++ MessageConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _ReadMessages();
+        }
+
+        private static void _ReadMessages()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter your message (200 char max):");
+                    var input = Console.ReadLine();
+                    var url = ConfigurationManager.AppSettings["MessageServiceEndpopint"];
+                    var client = new HttpClient();
+                    input = HttpUtility.HtmlEncode(input);
+                    var contentjson = new StringContent(string.Format("{{\"input\":\"{0}\"}}", input), UTF8Encoding.UTF8, "application/json");
+                    var response = client.PostAsync(url, contentjson).Result;
+                    response.EnsureSuccessStatusCode();
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    //TODO log exception
+                }
+            }
+        }
+    }
+}
---
+++ MessageConsoleListener/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+
+namespace MessageListener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _ListenForMessages();
+        }
+
+        private static void _ListenForMessages()
+        {
+            try
+            {
+                var server = new HttpListener();
+                var url = ConfigurationManager.AppSettings["MessageListenerEndpopint"];
+                server.Prefixes.Add(url);
+                server.Start();
+
+                Console.WriteLine("Listening for messages...");
+
+                while (true)
+                {
+                    var context = server.GetContext();
+                    var request = context.Request;
+
+                    string text;
+                    using (var reader = new StreamReader(request.InputStream, request.ContentEncoding))
+                    {
+                        text = reader.ReadToEnd();
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                //TODO log exception
+            }
+
+        }
+    }
+}
